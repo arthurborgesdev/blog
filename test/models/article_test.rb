d3e5128209b7cf080ca6a1cbc902d8563787fe64
arthurborgescc <arthur.borges@compartilhaclub.com.br>
2020-10-26 17:20:09 -0300

    Created tests about article and comments

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "article title should be greater or equal than 5 characters" do
+    article = Article.new
+    article.title = "Art" # ensures article.save to fail when validator length is present and set to 5
+    assert_not article.save, "Article has to be greater or equal 5 characters"
+  end
 end
+ 
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comment title can't be blank" do
+    article = Article.new
+    article.title = "Teste"
+    article.save
+
+    article = Article.find(article.id)
+    assert_not article.comments.create, "comment title can't be blank"
+  end
 end
