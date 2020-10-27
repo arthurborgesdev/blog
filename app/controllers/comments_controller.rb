e1b3546210aef9a80220f5891ee9eb4e0be6976b
arthurborgescc <arthur.borges@compartilhaclub.com.br>
2020-10-27 17:43:03 -0300

    Criada e finalizada a funcionalidade de editar comentários previamente criados

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,26 @@ class CommentsController < ApplicationController
 
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+  end
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to article_path(@article)
+    else
+      render 'comments/edit'
+    end
   end
 
   def destroy
